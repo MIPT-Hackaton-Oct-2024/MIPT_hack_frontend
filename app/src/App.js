b5af881755cc23875c215af51fb971e9e02b5f8b
AntonLED <al3406099@gmail.com>
2024-11-01 00:11:14 +0300

    [major update] Big update for frontend

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,7 @@ import "./index.css"
 import FileForm from './components/FileForm/FileForm';
 import { useState } from "react";
 import * as XLSX from 'xlsx';
+import axios from "axios";
 
 // import Header from './components/Load_data';
 // import LookData from './components/Look_on_data';
@@ -17,27 +18,193 @@ import aboutimage from './images/map.jpg';
 function App() {
  
   const [excelFile, setExcelFile] = useState(null);
-
   const [excelData, setExcelData] = useState(null);
 
-  const handleSubmit = async (e) => {
-      e.preventDefault();
-      const workbook = XLSX.read(excelFile, {type: 'buffer'});
-      const worksheetName = workbook.SheetNames[0];
-      const worksheet = workbook.Sheets[worksheetName];
-      const data = XLSX.utils.sheet_to_json(worksheet);
-      setExcelData(data.slice(0, 10));
+  // first part
+  const [strippedData, setStrippedData] = useState(null);
+  const [errorData, setErrorData] = useState(null);
+
+  // second part 
+  const [clustData, setClustData] = useState(null);
+  const [rawHTML, setRawHTML] = useState(null);
+
+  // third part 
+  const [lottingData, setLottingData] = useState(null);
+  const [metricsData, setMetricsData] = useState(null);
+
+  const fetchStrippedData = async (e) => {
+    const request = await fetch("/get_correct_data/"); 
+
+    const buff = await request.arrayBuffer();
+    console.log(buff);
+
+    const workbook = XLSX.read(new Uint8Array(buff), {type: 'array'});
+    const worksheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[worksheetName];
+    const data = XLSX.utils.sheet_to_json(worksheet);
+
+    setStrippedData(data.slice(0, 10));
   }
 
-  const handleFile = async (e) => {
-      let selectedFile = e.target.files[0];
+  const fetchErrorData = async (e) => {
+    const request = await fetch("/get_error_data/"); 
 
-      let reader = new FileReader();
-      reader.readAsArrayBuffer(selectedFile);
-      reader.onload = (e) => {
-        setExcelFile(e.target.result);
-      }
+    const buff = await request.arrayBuffer();
+    console.log(buff);
+
+    const workbook = XLSX.read(new Uint8Array(buff), {type: 'array'});
+    const worksheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[worksheetName];
+    const data = XLSX.utils.sheet_to_json(worksheet);
+
+    setErrorData(data.slice(0, 10));
   }
+
+  const fetchClusterMap = async (e) => {
+    const request = await fetch("/get_map/");
+    
+    const text = await request.text();
+
+    console.log(text);
+
+    setRawHTML(text);
+  }
+
+  const fetchLotting = async (e) => {
+    const request = await fetch("/get_lotting/"); 
+
+    const buff = await request.arrayBuffer();
+    console.log(buff);
+
+    const workbook = XLSX.read(new Uint8Array(buff), {type: 'array'});
+    const worksheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[worksheetName];
+
+    const data = XLSX.utils.sheet_to_json(worksheet);
+
+    // XLSX.writeFileAsync("./result.xlsx", workbook, (err) => {
+
+    // });
+
+    // XLSX.writeFile(workbook, )
+
+    setLottingData(data.slice(0, 10));
+  }
+
+  const fetchMetrics = async (e) => {
+    const request = await fetch("/get_metrix/");
+    
+    const loads = await request.json();
+
+    // console.log(loads);
+
+    document.getElementById("mett").innerHTML = loads;
+
+    // setMetricsData(loads);
+  }
+
+  const handleDownload = async (e) => {
+    alert("PIDORAS");
+
+    ///
+  }
+
+  const handleLotting = async (e) => {
+    const request = await fetch("/monte_carlo_lotting/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(
+            {
+                "Lambda": 0.02, 
+                "beta": 1.0
+            }
+        )
+    });
+
+    if (request.ok) {
+        console.log("Perform lotting");
+        //                 "filename": excelFile.name, 
+        await fetchLotting(e);
+        await fetchMetrics(e);
+    }
+    else {
+        console.error("SUCK MY BALLS jnkjn");
+    }
+  }
+
+  const performClusterize = async (e) => {
+    const request = await fetch("/clusterize/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({"max_cluster_size": 150, "linkage": "average"})
+    });
+
+    if (request.ok) {
+        console.log("Perform clusteroize");
+
+        await fetchClusterMap(e);
+
+        await handleLotting(e);
+    }
+    else {
+        console.error("SUCK MY BALLS");
+    }
+  }
+
+  const clearData = async (e) => {
+    const request = await fetch("/strip_data/", {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({"filename": excelFile.name})
+    });
+
+    if (request.ok) {
+        console.log("clearData");
+
+        await fetchStrippedData(e);
+        await fetchErrorData(e);
+
+        await performClusterize(e);
+    }
+    else {
+        console.error("FUCK");
+    }
+  }
+
+  const handleSubmitFile = async (e) => {
+    e.preventDefault();
+    // e.stopPropagation();
+
+    const formData = new FormData();
+    formData.append("file_upload", excelFile);
+
+    const response = await fetch("/uploadfile/", {
+        method: "POST",
+        body: formData
+    });
+
+    if (response.ok) {
+        console.log("File uploaded");
+        console.log(excelFile.name);
+        await clearData(e);
+    }
+    else {
+        console.error("File failed");
+    }
+
+  }
+
+  const handleFileForm = async (e) => {
+    setExcelFile(e.target.files[0]);
+  }
+
+
 
   return (
     <div className="App">
@@ -46,13 +213,16 @@ function App() {
       <div className='header'>
         <Navbar/>
 
-        <form className='intro' onSubmit={handleSubmit}>
+        <form className='intro' onSubmit={handleSubmitFile}>
             <h1>Загрузите <span> данные </span> и получите <span> лотирование </span></h1>
-            <input type="file"  required onChange={handleFile} />
-            <button className='header-btn' type="submit" >Загрузить</button>
+            <input type="file" onChange={handleFileForm} />
+            <button type="submit" className='header-btn'  >Загрузить</button>
             <p className="details"> в формате .xlsx, .csv</p>
+
+            <label>Максимальный размер кластера</label>
+            <imput type="number"/>
         </form>
-      </div>
+      </div> 
 
       {/* <Look_on_data/> */}
       <div className='lookdata'>
@@ -60,20 +230,20 @@ function App() {
                 <h2>Данные</h2>
                 <h4> Обрабатываемые заявки: </h4>
                 <div className="viewer" >
-                  {excelData?(
+                  {strippedData?(
                   <div >
                       <table >
 
                           <thead>
                               <tr>
-                              {Object.keys(excelData[0]).map((key)=>(
+                              {Object.keys(strippedData[0]).map((key)=>(
                                   <th key={key}>{key}</th>
                               ))}
                               </tr>
                           </thead>
 
                           <tbody>
-                              {excelData.map((individualExcelData, index)=>(
+                              {strippedData.map((individualExcelData, index)=>(
                               <tr key={index}>
                                   {Object.keys(individualExcelData).map((key)=>(
                                   <td key={key}>{individualExcelData[key]}</td>
@@ -91,20 +261,20 @@ function App() {
 
                 <h4> Небрабатываемые заявки (ошибки в данных): </h4>
                 <div className="viewer">
-                  {excelData?(
+                  {errorData?(
                   <div >
                       <table >
 
                           <thead>
                               <tr>
-                              {Object.keys(excelData[0]).map((key)=>(
+                              {Object.keys(errorData[0]).map((key)=>(
                                   <th key={key}>{key}</th>
                               ))}
                               </tr>
                           </thead>
 
                           <tbody>
-                              {excelData.map((individualExcelData, index)=>(
+                              {errorData.map((individualExcelData, index)=>(
                               <tr key={index}>
                                   {Object.keys(individualExcelData).map((key)=>(
                                   <td key={key}>{individualExcelData[key]}</td>
@@ -128,10 +298,7 @@ function App() {
       <div className='class'>
             <h1>Кластеризация</h1>
             <div className='class-model'>
-                <img src={aboutimage} alt='class image'/>
-            </div>
-            <div className='class-text'>
-                <h2>ГООООООООООООЙДААААААА</h2>
+                <iframe srcDoc={rawHTML}>Test</iframe>
             </div>
         </div>
 
@@ -139,19 +306,49 @@ function App() {
       <div className='lotting'>
           <h1>Лотирование</h1>
             <div className='lotting-model'>
-                <img src={aboutimage} alt='lotting image'/>
+            <div className="viewer" >
+                  {lottingData?(
+                  <div >
+                      <table >
+
+                          <thead>
+                              <tr>
+                              {Object.keys(lottingData[0]).map((key)=>(
+                                  <th key={key}>{key}</th>
+                              ))}
+                              </tr>
+                          </thead>
+
+                          <tbody>
+                              {lottingData.map((individualExcelData, index)=>(
+                              <tr key={index}>
+                                  {Object.keys(individualExcelData).map((key)=>(
+                                  <td key={key}>{individualExcelData[key]}</td>
+                                  ))}
+                              </tr>
+                              ))}
+                          </tbody>
+
+                      </table>
+                  </div>
+                  ):(
+                  <div>No File is uploaded yet!</div>
+                  )}
+                </div>
             </div>
-            {/* <div className='about-text'>
-                <h2>ГООООООООООООЙДААААААА</h2>
-                <button>Гойда</button>
-            </div> */}
+
+            <div className="metrics">
+            <div className="viewer">
+                  <div id="mett"></div>
+            </div>
+          </div>
         </div>
 
       {/* <Downloader/> */}
       <div className='download'>
           <p>Looking for shit?</p>
           <h1> Ваше <span> лотирование </span> готово</h1>
-          <button href='#' onClick={() => console.log("Bu ispugalsya??")} className='download-btn'>Скачать</button>
+          <button href='#' onClick={handleDownload} className='download-btn'>Скачать</button>
           <p className="details"> в формате .xlsx</p>
       </div>
 
